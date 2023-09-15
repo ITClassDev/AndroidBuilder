a4eb790ded4c1b23289bb0384ee50f889ffa6e9a
Slava Petrov <slavapmk@gmail.com>
2023-09-15 21:45:20 +0300

    Cumulative commit for transfer

--- src/main/kotlin/ru/shtp/androidbuilder/Main.kt
+++ src/main/kotlin/ru/shtp/androidbuilder/Main.kt
@@ -27,54 +27,61 @@ fun main() {
         println("Pull check")
         git.pull()
         val name = git.log().setMaxCount(1).call().iterator().next().name
-        if (Manager.appState.checkedVersion != name) {
-            println("New update available, building..")
-
-            if (File(Data.outputFolder).exists()) File(Data.outputFolder).deleteRecursively()
-            File(Data.outputFolder).mkdirs()
-
-            buildApplication()
-
-            val (versionName, versionCode) = FileReader(versionInfo).use {
-                val readLines = it.readLines()
-                Pair(readLines[0], readLines[1].toInt())
-            }
-            val releaseInfo = FileReader(releaseInfo).use { it.readText() }
-
-            FileWriter(tempReleaseManifest).use {
-                Manager.gson.toJson(
-                    ReleaseManifest(
-                        releaseInfo, System.currentTimeMillis(), versionName, versionCode
-                    ), it
-                )
-            }
-            println("Writed release manifwst")
-            File(tempReleaseManifest).copyTo(File(releaseFileManifest), true)
-            File(tempDebugApk).copyTo(File(releaseApk), true)
-            println("Copied files")
-            Manager.appState.checkedVersion = name
-            Manager.saveAppState()
-        }
+        if (Manager.appState.checkedVersion != name)
+            buildAll(name)
         println("Sleeping 10 minutes")
         Thread.sleep(600000)
     }
 }
 
-fun buildApplication(): Int {
+private fun buildAll(name: String) {
+    println("New update available, building..")
+    if (File(Data.outputFolder).exists()) File(Data.outputFolder).deleteRecursively()
+    File(Data.outputFolder).mkdirs()
+
+    val buildApplication = buildApk()
+    if (buildApplication != 0)
+        return
+
+    val (versionName, versionCode) = FileReader(versionInfo).use {
+        val readLines = it.readLines()
+        Pair(readLines[0], readLines[1].toInt())
+    }
+    val releaseInfo = FileReader(releaseInfo).use { it.readText() }
+
+    FileWriter(tempReleaseManifest).use {
+        Manager.gson.toJson(
+            ReleaseManifest(
+                releaseInfo, System.currentTimeMillis(), versionName, versionCode
+            ), it
+        )
+    }
+    println("Writed release manifwst")
+    File(tempReleaseManifest).copyTo(File(releaseFileManifest), true)
+    File(tempDebugApk).copyTo(File(releaseApk), true)
+    println("Copied files")
+    Manager.appState.checkedVersion = name
+    Manager.saveAppState()
+}
+
+fun buildApk(): Int {
     println("Start gradlew build")
 
     ProcessBuilder("chmod", "+x", "./gradlew")
         .directory(File(androidRepo))
         .redirectOutput(ProcessBuilder.Redirect.INHERIT).start().waitFor()
-    val process = ProcessBuilder("./gradlew", "build", "-Penv=production")
+    val waitVersions = ProcessBuilder("./gradlew", "generateVersions")
         .directory(File(androidRepo))
-        .redirectOutput(ProcessBuilder.Redirect.INHERIT).start()
-    val waitFor = process.waitFor()
-    ProcessBuilder("./gradlew", "generateVersions")
+        .redirectOutput(ProcessBuilder.Redirect.INHERIT).start().waitFor()
+    if (waitVersions != 0) {
+        println("Finished generating versions with code $waitVersions")
+        return waitVersions
+    }
+
+    val waitBuild = ProcessBuilder("./gradlew", "build", "--stacktrace")
         .directory(File(androidRepo))
         .redirectOutput(ProcessBuilder.Redirect.INHERIT).start().waitFor()
 
-    println("Finished build with code $waitFor")
-
-    return waitFor
+    println("Finished build with code $waitBuild")
+    return waitBuild
 }
